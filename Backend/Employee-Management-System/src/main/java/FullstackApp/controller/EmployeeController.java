748416baa81bce3bcb514d398b8ebc7c1334bf34
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2026-03-08 00:33:45 +0530

    Controller for to fethc allEmployees

---
+++ Backend/Employee-Management-System/src/main/java/FullstackApp/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package FullstackApp.controller;
+
+import FullstackApp.entity.Employee;
+import FullstackApp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
