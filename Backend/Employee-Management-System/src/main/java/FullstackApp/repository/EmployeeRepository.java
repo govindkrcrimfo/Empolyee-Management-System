4fad640f373a76682679cf0d84e38afb966a6103
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2026-03-07 22:42:37 +0530

    Backend Setup for Employee management System

---
+++ Backend/Employee-Management-System/src/main/java/FullstackApp/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package FullstackApp.repository;
+
+import FullstackApp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
