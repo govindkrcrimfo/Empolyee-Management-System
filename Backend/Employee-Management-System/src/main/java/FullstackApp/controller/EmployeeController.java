abde9f5a22ed53b7152b1d981c8972976949b251
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2026-03-15 00:51:46 +0530

    Routing added to get the list of employees

--- Backend/Employee-Management-System/src/main/java/FullstackApp/controller/EmployeeController.java
+++ Backend/Employee-Management-System/src/main/java/FullstackApp/controller/EmployeeController.java
@@ -3,11 +3,13 @@ package FullstackApp.controller;
 import FullstackApp.entity.Employee;
 import FullstackApp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins ="http://localhost:4200")
 @RestController
 public class EmployeeController {
     @Autowired
