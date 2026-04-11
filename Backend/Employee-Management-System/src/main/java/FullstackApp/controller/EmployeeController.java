cadd45191e4ea4c4e381ba2b0d203282565b7fba
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2026-04-12 00:56:09 +0530

    Update api created

--- Backend/Employee-Management-System/src/main/java/FullstackApp/controller/EmployeeController.java
+++ Backend/Employee-Management-System/src/main/java/FullstackApp/controller/EmployeeController.java
@@ -30,4 +30,14 @@ public class EmployeeController {
         Employee emp = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("employees not found with id = " + id));
         return ResponseEntity.ok(emp);
     }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        Employee employee1 = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employees not found with id = " + id + " to update "));
+        employee1.setFirstName(employee.getFirstName());
+        employee1.setLastName(employee.getLastName());
+        employee1.setSalary(employee.getSalary());
+        employeeRepository.save(employee1);
+        return ResponseEntity.ok(employee1);
+    }
 }
